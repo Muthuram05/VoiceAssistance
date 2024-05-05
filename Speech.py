3505f1464bbeff01ad1351617abcd58bad6810be
Muthu Raman <88555538+Muthuram05@users.noreply.github.com>
2024-05-05 11:43:30 +0530

    Update Speech.py

--- Speech.py
+++ Speech.py
@@ -1,40 +1,52 @@
-import pyttsx3
-import speech_recognition as sr
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
 import datetime
-import wikipedia
+import wikipedia #pip install wikipedia
+import webbrowser
 import os
-
-
+import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if hour>=0 and hour<12:
         speak("Good Morning!")
-    elif hour >= 12 and hour < 18:
-        speak("Good Afternoon!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
     else:
-        speak("Good Evening!")
-    speak("Please tell me may I help you")
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
 def takeCommand():
+    #It takes microphone input from the user and returns string output
+
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+
     try:
-        print("Recognizing...")
+        print("Recognizing...")    
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
+
     except Exception as e:
-        # print(e)
-        print("Say that again please...")
+        # print(e)    
+        print("Say that again please...")  
         return "None"
     return query
 
@@ -42,7 +54,10 @@ def takeCommand():
 if __name__ == "__main__":
     wishMe()
     while True:
+  
         query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -50,19 +65,42 @@ if __name__ == "__main__":
             speak("According to Wikipedia")
             print(results)
             speak(results)
-        elif 'tutorial' in query:
-            file = 'D:\my-app'
-            songs = os.listdir(file)
-            os.startfile(os.path.join(file, songs[1]))
 
-        elif 'open' in query:
-            file = 's:\\facedetect'
-            songs2 = os.listdir(file)
-            os.startfile(os.path.join(file, songs2[3]))
-        elif 'run' in query:
-            print("run")
-        elif 'set' in query:
-            print("set")
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            music_dir = 'G:\\kishan all.co\\d\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\kishancjx\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+            #you should enter your path here
+
+        elif 'email to ' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "YourfriendEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend. I am not able to send this email")
 
 
 
